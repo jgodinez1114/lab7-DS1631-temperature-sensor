90bc5bc9e408abad3c5148da7da7ced5554111b6
Joel Godinez <jgodinez1114@gmail.com>
2019-11-04 14:51:21 -0600

    Improved comments to improve readability

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* October 2019
+/* November 2019
  * Lab 7
  *
  */
@@ -23,15 +23,7 @@ DigitalOut col3(p6);
 DigitalOut col4(p5);
 
 //-----------Global Variables-----------//
-int first=0;// first number entered
-int temp1=0;//temporary variable used to store first number
-int second=0;//second number entered
-int temp2=0;//temporary variable used to store second number
-int third=0;//Third number entered
-int A=0;//first number for arithmetic operation
-int B=0;//second number for arithmetic operation
-int operand;//type of operation
-int counter=0;//counter used to keep track the number of input(limited to two)
+
 int arr[16][8] = {  {1,1,0,0,0,0,0,0}, //0 
 				{1,1,1,1,1,0,0,1}, //1 
 				{1,0,1,0,0,1,0,0}, //2 
@@ -59,9 +51,6 @@ void EdgeClock(){
   
 } 
 
-//function to parse the sensor reading to display
-
-
 //------------------Displays the values on the 7-segment--------------//
 void displayVal(int val1, int val2 , int val3){
 	//print third digit MS
@@ -80,11 +69,13 @@ void displayVal(int val1, int val2 , int val3){
 		EdgeClock(); 
 	}
 	
-
 	wait(0.2);// Paused to show 
 }
 
+//function to parse the sensor reading to display
+//work with unsigned reading
 void parseMyReading(unsigned int temp){
+	
 	int dig1, dig2, dig3, dig4;
 	float ctemp;
 	ctemp = temp * 0.0625;
@@ -92,10 +83,11 @@ void parseMyReading(unsigned int temp){
 	dig1 = int(temp)%10;
 	dig2 = temp/10;
 	
-	//call to display Celsius reading
+	//display Celsius reading
 	displayVal(dig1,dig2,12);
 	
 	float tempf;
+	
 	tempf = (temp * (1.8)) + 32;
 	
 	dig3 = int(tempf)%10;
@@ -103,7 +95,7 @@ void parseMyReading(unsigned int temp){
 	
 	wait(0.5);
 	
-	//call to display  Farenheit reading
+	//display  Farenheit reading
 	displayVal(dig3,dig4,15);
 	
 	wait(0.5);
@@ -114,54 +106,6 @@ void clearDisplay(){
 	displayVal(0,0,0);
 }
 
-
-
-
-
-
-//------------------Save the value of the key pressed-------------//
-void saveValue(int val){
-	// if number of digits is less than 2 and is a valid number 0-9, save the values for each digit
-	if (counter<2 && val<10){
-		temp1=first;
-		first=val;
-		temp2=second;
-		second=temp1;
-		third=temp2;
-		counter++;
-		displayVal(first,second, 0);
-	//if number is 10(A), 11(B), 12(C), 13(D), then save the previous digits as A, and save the current value as operand
-	}else if(val>9 && val != 14 && val != 15) {
-		A=(10*second)+first;
-		first=0;
-		second=0;
-		third=0;
-		counter=0;
-		displayVal(val,0,0);
-		operand=val;
-	//if number is F(15), then reset
-	}else if (val==15){
-		first=0;
-		second=0;
-		third=0;
-		counter=0;
-		displayVal(0,0,0);
-	//if value is E(14), then save the previous digits as B, reset first, second, third and counter
-  // and perform the correspondig operation
-	}else{
-		B=(10*second)+first;
-		first=0;
-		second=0;
-		third=0;
-		counter=0;
-		displayVal(val,0,0);
-		
-	}
-}
-
-
-
-
 int main(){ 
 	rst = 0; 
 	EdgeClock(); 
@@ -171,12 +115,13 @@ int main(){
 	
 	unsigned int temp;	
 	
-	
 	//start i2c communication using object
 	i2c.start();	
 	
+	//arbitrary address for communications
 	i2c.write(0x90);
 	
+	//access the 1-byte configuration register
 	i2c.write(0xAC);
 	
 	//configure itself to continuously monitor temperature
@@ -190,6 +135,7 @@ int main(){
 	
 	i2c.write(0x90);
 	
+	//initiate temperature conversions
 	i2c.write(0x51);
 	
 	i2c.stop();
@@ -201,29 +147,27 @@ int main(){
 	
 	i2c.write(0xAA);	
 	
+	//continuously read from sensor
 	while(1){
 	
+		//start communication
 		i2c.start();
 	
-		//read
+		//take reading
 		i2c.write(0x91);
 		
 		//get the first input
 		temp = i2c.read(0);
 		
+		//end comms
 		i2c.stop();
 		
+		//analyze reading from sensor
 		parseMyReading(temp);		
-		
-		//write
-		
-		//read
-		//NOte: work with HEX values per Mark Minton
-		//say goodbye
-		//Scudeira FERRARI BRO (vroom vroom)
+
+		//Scuderia FERRARI BRO (vroom vroom)
 	
-	}
-	
-}
+	}	
+}	//end main()
 
 
